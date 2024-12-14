5e3a1feae046afbb40420a64faa27385014c8984
standa <stanislav.brtna@gmail.com>
2024-12-14 20:59:52 +0100

    code cleanup

--- src/DRIVERS/usart3.c
+++ src/DRIVERS/usart3.c
@@ -11,14 +11,13 @@ uint32_t uart3BaudRate;
 void MX_USART3_UART_Init(void) {
   huart3.Instance = USART3;
   HAL_UART_DeInit(&huart3);
-  huart3.Init.BaudRate = uart3BaudRate;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.BaudRate     = uart3BaudRate;
+  huart3.Init.WordLength   = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits     = UART_STOPBITS_1;
+  huart3.Init.Parity       = UART_PARITY_NONE;
+  huart3.Init.Mode         = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl    = UART_HWCONTROL_NONE;
   huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-
 
   if (HAL_UART_Init(&huart3) != HAL_OK) {
     printf("Usart3 MX init error!!\n");
@@ -31,26 +30,11 @@ void MX_USART3_UART_Init(void) {
 void HAL_UART3_MspInit(UART_HandleTypeDef* uartHandle) {
   GPIO_InitTypeDef GPIO_InitStruct;
   (void) (uartHandle);
-  /* USER CODE BEGIN USART2_MspInit 0 */
-
-  /* USER CODE END USART2_MspInit 0 */
 	/* Peripheral clock enable */
 	__HAL_RCC_USART3_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
-	/**USART2 GPIO Configuration
-	PB10     ------> USART2_TX
-	PB11     ------> USART2_RX
-	*/
 
-	/*
-	HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
-	GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-	GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-	*/
+  // Pins are configured from the OS
 
 	HAL_NVIC_SetPriority(USART3_IRQn, 1, 1);
 	NVIC_ClearPendingIRQ(USART3_IRQn); // without this the thing borks...
@@ -60,17 +44,16 @@ void HAL_UART3_MspInit(UART_HandleTypeDef* uartHandle) {
   
 void MX_USART3_UART_DeInit(void){
     huart3.Instance = USART3;
-    __HAL_RCC_USART3_CLK_DISABLE();
-  
-    //HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
-
+    // if the clock is stopped, then the SDA borks on wakeup...
+    //__HAL_RCC_USART3_CLK_DISABLE();
 }
     
 void HAL_UART3_MspDeInit(UART_HandleTypeDef* uartHandle){
   if(uartHandle->Instance == USART3) {
-    __HAL_RCC_USART3_CLK_DISABLE();
+    //__HAL_RCC_USART3_CLK_DISABLE(); // same...
+    HAL_UART_AbortReceive_IT(&huart3);
+    NVIC_ClearPendingIRQ(USART3_IRQn);
     HAL_NVIC_DisableIRQ(USART3_IRQn);
-    //HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
   }
 }
 
@@ -207,7 +190,6 @@ uint8_t uart3_recieve_IT() {
   usart3_c = 0;
   usart3_DR = 0;
   usart3_buff_n = 0;
-
 
   if (!sdaSerialEnabled) {
     sda_serial_enable();
